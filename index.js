805aee37647e9588a8d8201ed4e3eef852a73628
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-06-18 07:45:53 +0600

    Chapter 03

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
+
+const app = new express();
 
 app.use(express.static('public'));
 
@@ -11,26 +12,27 @@ app.listen(3000, () => {
     console.log("App listening on port 3000");
 });
 
-app.get("/json", (req, res) => {
-    res.json({
-        name: 'Greg Lim'
-    })
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
 });
 
 
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
 });
 
 
-app.get("/about", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'about.html'));
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
 });
 
 
-app.get("/contact", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'contact.html'));
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
 });
+
 
 
 
