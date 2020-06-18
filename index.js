9e096da913529c7c7487809d974da8de1cddb950
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-06-18 06:39:10 +0600

    Chapter 02

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
 
-const homePage = fs.readFileSync('home.html');
-const aboutPage = fs.readFileSync('about.html');
-const contactPage = fs.readFileSync('contact.html');
-const notFoundPage = fs.readFileSync('notfound.html');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/about')
-        res.end(aboutPage);
-    else if(req.url === '/contact')
-        res.end(contactPage);
-    else if(req.url === '/')
-        res.end(homePage);
-    else {
-        res.writeHead(404);
-        res.end(notFoundPage);
-    }
-})
+app.use(express.static('public'));
 
-server.listen(3000);
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000");
+});
+
+app.get("/json", (req, res) => {
+    res.json({
+        name: 'Greg Lim'
+    })
+});
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'about.html'));
+});
+
+
+app.get("/contact", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'contact.html'));
+});
+
+
+
