14a546cb9e026e012644eadea2ade9257c38c8a2
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-06-18 21:01:29 +0600

    Chapter 06

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
 
 const app = new express();
 
+const BlogPost = require('./models/BlogPost.js');
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extendex:true}));
 
 
 app.listen(3000, () => {
@@ -18,8 +24,11 @@ app.listen(3000, () => {
 
 
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const blogposts = await BlogPost.find({});
+    res.render('index', {
+        blogposts
+    });
 });
 
 
@@ -29,15 +38,29 @@ app.get('/about', (req, res) => {
 });
 
 
-app.get('/post', (req, res) => {
-    // res.sendFile(path.resolve(__dirname, 'pages/post.html'));
-    res.render('post');
+app.get('/post/:id', async (req, res) => {
+    const blogpost = await BlogPost.findById(req.params.id);
+    res.render('post', {
+        blogpost
+    });
+    console.log(req.params);
 });
 
 
 app.get('/contact', (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
     res.render('contact');
+});
+
+
+app.get('/posts/new', (req, res) => {
+    res.render('create');
+});
+
+
+app.post('/posts/store', async (req, res) => {
+    await BlogPost.create(req.body);
+    res.redirect('/');
 });
 
 
--- models/BlogPost.js
+++ models/BlogPost.js
@@ -4,7 +4,12 @@ const Schema = mongoose.Schema;
 
 const BlogPostSchema = new Schema({
     title: String,
-    body: String
+    body: String,
+    username: String,
+    datePosted: {
+        type: Date,
+        default: new Date()
+    }
 });
 
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
