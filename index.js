d77cec98b46d15e072a6271bce9f33c71e4d1cb5
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-06-18 08:45:50 +0600

    Chapter 04

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 const express = require('express');
 const path = require('path');
-
+const ejs = require('ejs');
 
 const app = new express();
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 
 app.listen(3000, () => {
@@ -15,22 +16,25 @@ app.listen(3000, () => {
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    res.render('index');
 });
 
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+    // res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+    res.render('about');
 });
 
 
 app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+    // res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+    res.render('post');
 });
 
 
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    // res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    res.render('contact');
 });
 
 
