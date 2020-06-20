bdfc627c6b6484c4ca579f854f02d530b4c2e1e9
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-06-20 12:52:13 +0600

    Chapter 14

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -3,7 +3,6 @@ const BlogPost = require('../models/BlogPost.js');
 
 module.exports = async (req, res) => {
     const blogpost = await BlogPost.findById(req.params.id).populate('userid');
-    console.log(blogpost);
     res.render('post', {
         blogpost
     });
